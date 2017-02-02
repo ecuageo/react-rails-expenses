1dc02acaf57ddc81f3b48f214b9b7e7cd15a8334
SANTIAGO PEREZ <perezperret@gmail.com>
2017-02-01 22:56:35 -0400

    allow empty end_time field

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,4 @@
 class Record < ApplicationRecord
   validates_datetime :start_time
-  validates_datetime :end_time, after: :start_time
+  validates_datetime :end_time, after: :start_time, allow_blank: true
 end
