b02e4ecdfcfff0e609d312f6a5d54511a1a4e2c6
SANTIAGO PEREZ <perezperret@gmail.com>
2017-02-08 17:42:40 -0400

    experiment with server side rendering

--- config/initializers/react_rails.rb
+++ config/initializers/react_rails.rb
@@ -1,4 +1,5 @@
 # Disable react-rails sprockets strategy to prevent react compilation
-
 Rails.application.config.react.sprockets_strategy = false
+
+# Convert props object from ruby-style snake_case to js-style camelCase
 Rails.application.config.react.camelize_props = true
